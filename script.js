adfdba22fb3bd5cd2c42a15ba05fa650cab3f367
lucianobs1 <lucianotetsu@gmail.com>
2020-05-27 15:21:35 -0300

    tornando videos dinamicos

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 const currentPage = document.location.pathname;
-const menuItems = document.querySelectorAll("header a")
+const menuItems = document.querySelectorAll("header a");
 
 for(item of menuItems){
   if(currentPage.includes(item.getAttribute("href"))){
     item.classList.add("active");
   }
 }
+
+const modalOverlay = document.querySelector(".modal-overlay");
+const videos = document.querySelectorAll(".video");
+
+for (let video of videos){
+  video.addEventListener("click", function(){
+    const videoKey = video.getAttribute("id")
+    modalOverlay.classList.add('active-modal');
+    modalOverlay.querySelector("iframe").src = `https://www.blogger.com/video.g?token=${videoKey}`;
+
+
+  });
+}
+
+document.querySelector(".close-modal").addEventListener('click', function(){
+  modalOverlay.classList.remove('active-modal');
+  modalOverlay.querySelector("iframe").src = "";
+});
 
 
 
